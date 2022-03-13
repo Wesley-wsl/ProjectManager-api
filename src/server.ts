96bcca99665aba356f87f9c6ca584072a4ad5346
Wesley Westelley <wesleywestelley@gmail.com>
2022-03-13 00:57:07 -0300

    :wrench: feat: add routes for user and client

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,15 @@ import express from "express";
 
 import "./api/database";
 import { errorHandler } from "./api/middlewares/errorHandler";
+import { userRouter, clientRouter } from "./api/routes";
 
 config();
 
 const app = express();
 
 app.use(express.json());
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/client", clientRouter);
 app.use(errorHandler);
 
 app.listen(3333, () => console.log("Server is running"));
