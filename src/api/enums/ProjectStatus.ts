140f853c415a706fcc9fc7d295feb57ddca19f84
Wesley Westelley <wesleywestelley@gmail.com>
2022-03-17 16:20:12 -0300

    :white_check_mark: chore: create enums for project status

---
+++ src/api/enums/ProjectStatus.ts
@@ -0,0 +1,8 @@
+/* eslint-disable no-shadow */
+export enum IProjectStatus {
+    NEW = "NEW",
+    IN_PROGRESS = "IN_PROGRESS",
+    PAUSE = "PAUSE",
+    FINISHED = "FINISHED",
+    CANCELED = "CANCELED",
+}
