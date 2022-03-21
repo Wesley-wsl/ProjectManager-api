d31b08a2b9f48718dd622eeb80a8364fb75e61be
Wesley Westelley <wesleywestelley@gmail.com>
2022-03-21 18:13:30 -0300

    :white_check_mark: feat: create router with documentation

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,12 @@ import cors from "cors";
 import { config } from "dotenv";
 import express from "express";
 import { resolve } from "path";
+import swaggerUi from "swagger-ui-express";
 
 import "./api/database";
 import { errorHandler } from "./api/middlewares/errorHandler";
 import { userRouter, clientRouter, projectRouter } from "./api/routes";
+import swaggerJSON from "./config/swagger/index.json";
 
 config();
 
@@ -16,9 +18,10 @@ app.use(cors());
 app.use("/files", express.static(resolve(__dirname, "..", "uploads")));
 
 app.use(express.json());
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerJSON));
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/client", clientRouter);
 app.use("/api/v1/project", projectRouter);
 app.use(errorHandler);
 
-app.listen(3333, () => console.log("Server is running"));
+app.listen(process.env.PORT || 3333, () => console.log("Server is running"));
