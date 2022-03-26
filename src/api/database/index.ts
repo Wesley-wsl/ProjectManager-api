23973dc6376b61683f52988d1c390938336b66e7
Wesley Westelley <wesleywestelley@gmail.com>
2022-03-26 17:18:30 -0300

    :recycle: refactor: create connection

--- src/api/database/index.ts
+++ src/api/database/index.ts
@@ -1,10 +1,3 @@
 import { createConnection } from "typeorm";
 
-if (process.env.DATABASE_URL) {
-    createConnection({
-        type: "postgres",
-        url: `${process.env.DATABASE_URL}`,
-    });
-} else {
-    createConnection();
-}
+createConnection();
